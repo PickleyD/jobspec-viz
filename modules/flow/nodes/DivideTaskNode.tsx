e12c177f1c8c3c5f84e0d1a8e2b5f3be4be3081b
Matt Durkin <m.durkin92@gmail.com>
2022-05-15 15:47:05 +0100

    Add PowerTextField component

--- modules/flow/nodes/DivideTaskNode.tsx
+++ modules/flow/nodes/DivideTaskNode.tsx
@@ -2,6 +2,7 @@ import { TaskNode } from "./TaskNode";
 import { NodeProps } from "react-flow-renderer";
 import React from "react";
 import { useSelector } from "@xstate/react";
+import { PowerTextField } from "./fields";
 
 const incomingNodesSelector = (state: any) => state.context.incomingNodes;
 const inputSelector = (state: any) => state.context.taskSpecific.input;
@@ -17,46 +18,26 @@ export const DivideTaskNode = (nodeProps: NodeProps) => {
 
     const incomingNodes = useSelector(machine, incomingNodesSelector);
 
-    console.log(incomingNodes)
-
     return (
         <TaskNode {...nodeProps}>
-            <div className="form-control w-full max-w-xs">
-                <label className="label">
-                    <span className="label-text">Input</span>
-                </label>
-                <input
-                    value={input}
-                    onChange={(event) => machine.send("SET_TASK_SPECIFIC_PROPS", { value: { input: event.target.value } })}
-                    type="text"
-                    placeholder="Type here"
-                    className="input input-bordered w-full max-w-xs"
-                />
-            </div>
-            <div className="form-control w-full max-w-xs">
-                <label className="label">
-                    <span className="label-text">Divisor</span>
-                </label>
-                <input
-                    value={divisor}
-                    onChange={(event) => machine.send("SET_TASK_SPECIFIC_PROPS", { value: { divisor: event.target.value } })}
-                    type="text"
-                    placeholder="Type here"
-                    className="input input-bordered w-full max-w-xs"
-                />
-            </div>
-            <div className="form-control">
-                <label className="label">
-                    <span className="label-text">Precision</span>
-                </label>
-                <input
-                    value={precision}
-                    onChange={(event) => machine.send("SET_TASK_SPECIFIC_PROPS", { value: { precision: event.target.value } })}
-                    type="text"
-                    placeholder="Type here"
-                    className="input input-bordered w-full max-w-xs"
-                />
-            </div>
+            <PowerTextField
+                label="Input"
+                value={input}
+                onChange={(event) => machine.send("SET_TASK_SPECIFIC_PROPS", { value: { input: event.target.value } })}
+                incomingNodes={incomingNodes}
+            />
+            <PowerTextField
+                label="Divisor"
+                value={divisor}
+                onChange={(event) => machine.send("SET_TASK_SPECIFIC_PROPS", { value: { divisor: event.target.value } })}
+                incomingNodes={incomingNodes}
+            />
+            <PowerTextField
+                label="Precision"
+                value={precision}
+                onChange={(event) => machine.send("SET_TASK_SPECIFIC_PROPS", { value: { precision: event.target.value } })}
+                incomingNodes={incomingNodes}
+            />
         </TaskNode>
     );
 };
---
+++ modules/flow/nodes/fields/PowerTextField.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export interface PowerTextFieldProps {
+    label: string;
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    incomingNodes: Array<string>;
+}
+
+export const PowerTextField = ({
+    label,
+    value,
+    onChange,
+    incomingNodes
+}: PowerTextFieldProps) => {
+
+    return <div className="form-control w-full max-w-xs">
+        <label className="label pb-0">
+            <span className="label-text">{label}</span>
+        </label>
+        <div className="flex items-center gap-1">
+            <input
+                value={value}
+                onChange={onChange}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+            />
+            <div className="dropdown">
+                <label tabIndex={0} className=" m-1">
+                    <button className="btn btn-circle btn-outline h-8 w-8 min-h-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                        </svg>
+                    </button>
+                </label>
+                <ul tabIndex={0} className="border dropdown-content menu p-2 shadow bg-base-100 rounded w-52">
+                    {
+                        incomingNodes && incomingNodes.length > 0 ?
+                            incomingNodes.map((incomingNode: string) => <li className="text-xs"><a>{incomingNode}</a></li>)
+                            :
+                            <li className="text-xs">No incoming tasks connected</li>
+                    }
+                </ul>
+            </div>
+        </div>
+    </div>
+}
