397911a1cbda75f0dbb7fea64ec429f7caef0bdb
Matt Durkin <m.durkin92@gmail.com>
2022-05-18 18:46:07 +0100

    Fix warnings

--- modules/flow/nodes/fields/PowerTextField.tsx
+++ modules/flow/nodes/fields/PowerTextField.tsx
@@ -36,15 +36,15 @@ export const PowerTextField = ({
             <div className="dropdown">
                 <label tabIndex={0} className=" m-1">
                     <button className="btn btn-circle btn-outline h-8 w-8 min-h-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" />
                         </svg>
                     </button>
                 </label>
                 <ul tabIndex={0} className="border dropdown-content menu p-2 shadow bg-base-100 rounded w-52">
                     {
                         incomingNodes && incomingNodes.length > 0 ?
-                            incomingNodes.map((incomingNode: string) => <li className="text-xs"><a onClick={() => handleIncomingNodeSelected(incomingNode)}>{incomingNode}</a></li>)
+                            incomingNodes.map((incomingNode: string, index: number) => <li key={index} className="text-xs"><a onClick={() => handleIncomingNodeSelected(incomingNode)}>{incomingNode}</a></li>)
                             :
                             <li className="text-xs">No incoming tasks connected</li>
                     }
