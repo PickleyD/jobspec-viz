2f480d9480a05f4e291f639bb7176ed430a8cde9
Matt Durkin <m.durkin92@gmail.com>
2022-10-07 14:38:18 +0100

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
       // implement node event listeners here
     },
     video: false,
-    screenshotOnRunFailure: false
+    screenshotOnRunFailure: false,
+    supportFile: false
   },
 });
