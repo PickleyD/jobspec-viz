ce660ca712402c384fc8d2a951c4bb041cc7e7f5
Matt Durkin <m.durkin92@gmail.com>
2022-10-21 19:27:18 +0100

    Animate an HTML element along the path between a completed task and the next one

--- modules/flow/CustomEdge.tsx
+++ modules/flow/CustomEdge.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { EdgeProps, getBezierPath } from 'react-flow-renderer';
+import { EdgeProps, getBezierEdgeCenter, getBezierPath } from 'react-flow-renderer';
 import { useSelector } from "@xstate/react";
 import { GlobalStateContext } from "../../context/GlobalStateContext";
 import { useEffect } from "react";
@@ -11,6 +11,13 @@ const sourceTaskRunResultSelector = (state: any, sourceId?: string) => {
 
     return fromNodeTaskMachine?.state.context.runResult
 }
+
+const foreignObjectSize = 30;
+
+const onEdgeClick = (evt: any, id: string) => {
+    evt.stopPropagation();
+    alert(`remove ${id}`);
+};
 
 export const CustomEdge = ({
     id,
@@ -34,6 +41,15 @@ export const CustomEdge = ({
         targetPosition,
     });
 
+    const [labelX, labelY] = getBezierEdgeCenter({
+        sourceX,
+        sourceY,
+        sourcePosition,
+        targetX,
+        targetY,
+        targetPosition,
+    });
+
     const globalServices = useContext(GlobalStateContext);
 
     const sourceTaskRunResult = useSelector(
@@ -47,26 +63,31 @@ export const CustomEdge = ({
 
     return (
         <>
-            <path
-                id={id}
-                style={style}
-                className="react-flow__edge-path"
-                d={edgePath}
-                markerEnd={markerEnd}
-            />
-            {
-                sourceTaskRunResult &&
-                <text>
-                    <textPath
-                        href={`#${id}`}
-                        style={{ fontSize: '12px' }}
-                        startOffset="50%"
-                        textAnchor="middle"
-                    >
-                        {JSON.stringify(sourceTaskRunResult)}
-                    </textPath>
-                </text>
-            }
+            <svg xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                    <path id={id} d={edgePath} />
+                </defs>
+                <use href={`#${id}`} className="react-flow__edge-path" />
+                {sourceTaskRunResult && <svg xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <foreignObject
+                            requiredExtensions="http://www.w3.org/1999/xhtml"
+                            width={foreignObjectSize}
+                            height={foreignObjectSize}
+                            transform={`translate(-${foreignObjectSize / 2} -${foreignObjectSize / 2})`}>
+                            <div xmlns="http://www.w3.org/1999/xhtml" className="h-full w-full flex items-center justify-center bg-secondary rounded-full">
+                                <button className="edgebutton" onClick={(event) => onEdgeClick(event, id)}>
+                                    ×
+                                </button>
+                            </div>
+                        </foreignObject>
+                        <animateMotion id="motion" rotate="0" begin="0s" dur="500ms"
+                            end="250ms" keyPoints=".50;.50" keyTimes="0;1" fill="freeze">
+                            <mpath href={`#${id}`} />
+                        </animateMotion>
+                    </g>
+                </svg>}
+            </svg>
         </>
     );
 }
