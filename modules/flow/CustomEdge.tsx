9baecf5d90cd3314e402ab126654e4e77dc1b28d
Matt Durkin <m.durkin92@gmail.com>
2022-10-24 11:39:19 +0100

    Add VarsDisplay component and use inside CustomEdge

--- modules/flow/CustomEdge.tsx
+++ modules/flow/CustomEdge.tsx
@@ -2,9 +2,9 @@ import React, { useContext } from 'react';
 import { EdgeProps, getBezierEdgeCenter, getBezierPath } from 'react-flow-renderer';
 import { useSelector } from "@xstate/react";
 import { GlobalStateContext } from "../../context/GlobalStateContext";
-import { useEffect } from "react";
 import { EyeIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Popover } from '../../components';
+import { VarsDisplay } from './varsDisplay/VarsDisplay';
 
 const sourceTaskRunResultSelector = (state: any, sourceId?: string) => {
     if (!sourceId) return
@@ -15,11 +15,6 @@ const sourceTaskRunResultSelector = (state: any, sourceId?: string) => {
 }
 
 const foreignObjectSize = 100;
-
-const onEdgeClick = (evt: any, id: string) => {
-    evt.stopPropagation();
-    alert(`remove ${id}`);
-};
 
 export const CustomEdge = ({
     id,
@@ -59,41 +54,35 @@ export const CustomEdge = ({
         (state: any) => sourceTaskRunResultSelector(state, source)
     );
 
-    useEffect(() => {
-        console.log(sourceTaskRunResult)
-    }, [sourceTaskRunResult])
-
     return (
-        <>
-            <svg xmlns="http://www.w3.org/2000/svg">
-                <defs>
-                    <path id={id} d={edgePath} />
-                </defs>
-                <use href={`#${id}`} className="react-flow__edge-path" />
-                {sourceTaskRunResult && <svg xmlns="http://www.w3.org/2000/svg">
-                    <g>
-                        <foreignObject
-                            width={foreignObjectSize}
-                            height={foreignObjectSize}
-                            transform={`translate(-${foreignObjectSize / 2} -${foreignObjectSize / 2})`}>
-                            <div className="h-full w-full flex items-center justify-center">
-                                <Popover label={(open) => <label
-                                    tabIndex={0}
-                                    className={`shadow-widget border-gray-800 border-2 focus:border-2 fous:border-secondary hover:border-2 hover:border-secondary focus:border-secondary bg-base-100 h-8 w-8 min-h-0 btn btn-circle swap swap-rotate ${open ? "swap-active" : ""}`}
-                                >
-                                    <EyeIcon className="swap-off h-5 w-5 text-white" />
-                                    <XMarkIcon className="swap-on h-5 w-5 text-white" />
-                                </label>}
-                                    content={<div className="max-w-sm p-4">the content</div>} />
-                            </div>
-                        </foreignObject>
-                        <animateMotion id="motion" rotate="0" begin="0s" dur="500ms"
-                            end="250ms" keyPoints=".50;.50" keyTimes="0;1" fill="freeze">
-                            <mpath href={`#${id}`} />
-                        </animateMotion>
-                    </g>
-                </svg >}
-            </svg >
-        </>
+        <svg xmlns="http://www.w3.org/2000/svg">
+            <defs>
+                <path id={id} d={edgePath} />
+            </defs>
+            <use href={`#${id}`} className="react-flow__edge-path" />
+            {sourceTaskRunResult && <svg xmlns="http://www.w3.org/2000/svg">
+                <g>
+                    <foreignObject
+                        width={foreignObjectSize}
+                        height={foreignObjectSize}
+                        transform={`translate(-${foreignObjectSize / 2} -${foreignObjectSize / 2})`}>
+                        <div className="h-full w-full flex items-center justify-center">
+                            <Popover label={(open) => <label
+                                tabIndex={0}
+                                className={`shadow-widget border-gray-800 border-2 focus:border-2 fous:border-secondary hover:border-2 hover:border-secondary focus:border-secondary bg-base-100 h-8 w-8 min-h-0 btn btn-circle swap swap-rotate ${open ? "swap-active" : ""}`}
+                            >
+                                <EyeIcon className="swap-off h-5 w-5 text-white" />
+                                <XMarkIcon className="swap-on h-5 w-5 text-white" />
+                            </label>}
+                                content={<VarsDisplay vars={sourceTaskRunResult.Vars}/>} />
+                        </div>
+                    </foreignObject>
+                    <animateMotion id="motion" rotate="0" begin="0s" dur="500ms"
+                        end="250ms" keyPoints=".50;.50" keyTimes="0;1" fill="freeze">
+                        <mpath href={`#${id}`} />
+                    </animateMotion>
+                </g>
+            </svg>}
+        </svg>
     );
 }
---
+++ modules/flow/varsDisplay/VarsDisplay.tsx
@@ -0,0 +1,41 @@
+import { PlusIcon, MinusIcon } from "@heroicons/react/24/outline"
+import { useState } from "react"
+
+export interface VarsDisplayProps {
+    vars: { [key: string]: string }
+}
+
+const SHOW_TRUNCATOR_CHARS_LIMIT = 150
+
+export const VarsDisplay = ({ vars }: VarsDisplayProps) => {
+
+    const [shrink, setShrink] = useState<boolean>(true)
+
+    const toggleShrink = () => setShrink(!shrink)
+
+    return <div className="p-4 max-w-sm flex flex-col gap-2">
+        <h4 className="text-sm font-bold">Vars Snapshot</h4>
+        <ul className="text-xs flex flex-col gap-2">
+            {Object.keys(vars).map(key => {
+
+                const showTruncator = vars[key].length > SHOW_TRUNCATOR_CHARS_LIMIT
+
+                return <li className="flex flex-col gap-1">
+                    <p className="font-bold text-secondary whitespace-nowrap">{key}:</p>
+                    <div className="flex flex-row items-start w-full">
+                        {showTruncator
+                            ?
+                            <button className={`basis-4 py-[2px] grow-0 items-start justify-start swap swap-rotate ${shrink ? "swap-active" : ""}`}>
+                                <PlusIcon className="swap-off h-3 w-3 stroke-secondary" onClick={toggleShrink} />
+                                <MinusIcon className="swap-on h-3 w-3 stroke-secondary" onClick={toggleShrink} />
+                            </button>
+                            :
+                            <div className="basis-4 grow-0"></div>
+                        }
+                        <p className={`min-w-0 basis-full grow-0 break-words ${shrink ? "text-ellipsis whitespace-nowrap overflow-hidden" : ""}`}>{vars[key]}</p>
+                    </div>
+                </li>
+            })}
+        </ul>
+    </div>
+}
