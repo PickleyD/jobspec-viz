fe7d4abe3eea7665b3cb2e605454340de9e69313
MATTOP\Matthew <m.durkin92@gmail.com>
2022-11-15 23:22:24 +0000

    Change styling of mobile support msg

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,42 +8,47 @@ import { useState, useContext } from "react";
 import {
   Bars3Icon,
   BookOpenIcon,
-  XMarkIcon
+  XMarkIcon,
 } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import { Simulator } from "../modules/simulator";
-import { GlobalStateContext } from "../context/GlobalStateContext"
-import empty from "../examples/empty.json"
-import getUint256 from "../examples/getUint256.json"
+import { GlobalStateContext } from "../context/GlobalStateContext";
+import empty from "../examples/empty.json";
+import getUint256 from "../examples/getUint256.json";
 import { useSelector } from "@xstate/react";
 
-const reactFlowInstanceSelector = (state: any) => state.context.reactFlowInstance;
+const reactFlowInstanceSelector = (state: any) =>
+  state.context.reactFlowInstance;
 
 const Home: NextPage = () => {
-
   const [helpMsgDisplayed, setHelpMsgDisplayed] = useState<boolean>(true);
 
-  const [newProjectHeroDisplayed, setNewProjectHeroDisplayed] = useState<boolean>(true);
+  const [newProjectHeroDisplayed, setNewProjectHeroDisplayed] =
+    useState<boolean>(true);
 
-  const handleNewEmptyProject = () => setNewProjectHeroDisplayed(false)
+  const handleNewEmptyProject = () => setNewProjectHeroDisplayed(false);
 
   const globalServices = useContext(GlobalStateContext);
 
   const reactFlowInstance = useSelector(
     globalServices.workspaceService,
     reactFlowInstanceSelector
-  )
+  );
 
   const handleRehydrate = (json: any) => {
     globalServices.workspaceService.send("RESTORE_STATE", {
       savedContext: json,
     });
-    setNewProjectHeroDisplayed(false)
-    setTimeout(() => reactFlowInstance.fitView({
-      duration: 500,
-      padding: 1
-    }), 100)
-  }
+    setNewProjectHeroDisplayed(false);
+    setTimeout(
+      () =>
+        reactFlowInstance.fitView({
+          duration: 500,
+          padding: 1,
+        }),
+      100
+    );
+  };
 
   return (
     <div className="bg-primary h-screen w-screen">
@@ -55,7 +60,7 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {newProjectHeroDisplayed &&
+      {newProjectHeroDisplayed && (
         <div className="absolute z-50 h-full w-full pointer-events-auto backdrop-blur-sm flex items-center justify-center p-8">
           <div className="bg-base-100 relative shadow-widget h-full w-full max-w-[800px] max-h-[500px] rounded-lg flex flex-col items-start justify-start gap-8">
             <div className="absolute top-4 right-4 h-96 w-96 z-10">
@@ -82,25 +87,44 @@ const Home: NextPage = () => {
                 <div className="relative h-20 w-[141px]">
                   <Image src="/linkit.svg" alt="linkit logo" layout="fill" />
                 </div>
-                <h2 className="text-gray-300 text-sm font-bold max-w-sm">Learn, create, and test your next Chainlink job spec — <span className="text-secondary">directly from your web browser</span></h2>
+                <h2 className="text-gray-300 text-sm font-bold max-w-sm">
+                  Learn, create, and test your next Chainlink job spec —{" "}
+                  <span className="text-secondary">
+                    directly from your web browser
+                  </span>
+                </h2>
               </div>
               <div className="divider max-w-[400px]" />
               <div className="flex flex-col gap-2">
                 <div className="">
-                  <p className="text-gray-300 text-sm font-bold max-w-sm">Start a new empty project or pick an example to give yourself a headstart</p>
+                  <p className="text-gray-300 text-sm font-bold max-w-sm">
+                    Start a new empty project or pick an example to give
+                    yourself a headstart
+                  </p>
                 </div>
                 <div className="z-20 flex items-center justify-center h-28">
-                  <div className="btn btn-outline" onClick={() => handleRehydrate(empty)}>Empty Project</div>
+                  <div
+                    className="btn btn-outline"
+                    onClick={() => handleRehydrate(empty)}
+                  >
+                    Empty Project
+                  </div>
                   <div className="divider divider-horizontal"></div>
-                  <div className="btn btn-outline" onClick={() => handleRehydrate(getUint256)}>{`Get -> Uint256`}</div>
+                  <div
+                    className="btn btn-outline"
+                    onClick={() => handleRehydrate(getUint256)}
+                  >{`Get -> Uint256`}</div>
                   <div className="divider divider-horizontal"></div>
-                  <div className="btn btn-outline" onClick={() => handleRehydrate(getUint256)}>{`Get -> Uint256`}</div>
+                  <div
+                    className="btn btn-outline"
+                    onClick={() => handleRehydrate(getUint256)}
+                  >{`Get -> Uint256`}</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      }
+      )}
       {/* 
       {helpMsgDisplayed && (
         <div className="invisible md:visible fixed grid items-center justify-center h-full w-full z-10 pointer-events-none">
@@ -156,13 +180,10 @@ const Home: NextPage = () => {
         <div className="w-full h-full fixed z-0">
           <Flow />
         </div>
-        <div className="visible md:invisible fixed grid items-center justify-center h-full w-full z-10 pointer-events-none">
-          <div className="rounded bg-gray-700 p-4 flex flex-col gap-4 relative pointer-events-auto w-80">
-            <div className="flex flex-col items-center gap-2">
-              <Image alt="logo" src="/logo.png" width={40} height={40} />
-              <h1 className="font-bold underline text-xl">
-                Chainlink Job Spec Viz
-              </h1>
+        <div className="visible md:invisible fixed grid items-center justify-center h-full w-full z-50 pointer-events-auto bg-primary">
+          <div className="rounded-lg bg-base-100 p-8 flex flex-col gap-4 relative pointer-events-auto w-80">
+            <div className="relative h-20 w-[141px]">
+              <Image src="/linkit.svg" alt="linkit logo" layout="fill" />
             </div>
             <div className="text-xs max-w-sm flex flex-col gap-2">
               <p>
