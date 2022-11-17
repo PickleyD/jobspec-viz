bd8b7c942027e14903fb64c581a303bd7071c0ff
MATTOP\Matthew <m.durkin92@gmail.com>
2022-11-17 20:26:39 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,10 +53,10 @@ const Home: NextPage = () => {
   return (
     <div className="bg-primary h-screen w-screen">
       <Head>
-        <title>Chainlink Job Spec Viz</title>
+        <title>LINKIT</title>
         <meta
           name="description"
-          content="Visual Programming for Chainlink Job Specifications"
+          content="Visually Create and Test Your Next Chainlink Job Spec"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -125,57 +125,6 @@ const Home: NextPage = () => {
           </div>
         </div>
       )}
-      {/* 
-      {helpMsgDisplayed && (
-        <div className="invisible md:visible fixed grid items-center justify-center h-full w-full z-10 pointer-events-none">
-          <div className="rounded rounded-tr-3xl bg-base-100 p-4 flex flex-col gap-4 relative pointer-events-auto shadow-widget">
-            <label
-              tabIndex={0}
-              onClick={() => setHelpMsgDisplayed(false)}
-              className={`pointer-events-auto absolute z-10 right-0 top-0 btn border-0 hover:border-2 hover:border-secondary btn-circle`}
-            >
-              <XMarkIcon className="swap-on fill-current h-5 w-5 text-white" />
-            </label>
-            <div className="flex items-center gap-2">
-              <Image alt="logo" src="/logo.png" width={80} height={80} />
-              <h1 className="font-bold underline text-xl">
-                Chainlink Job Spec Viz
-              </h1>
-            </div>
-            <div className="text-xs max-w-sm flex flex-col gap-2">
-              <p>
-                Drag, drop and connect tasks to visually program your Chainlink
-                job spec. Copy the generated code to your Chainlink node and
-                you&apos;re good to go!
-              </p>
-              <p>Here are the steps to follow:</p>
-              <ul className="list-decimal pl-8">
-                <li className="py-1">
-                  Select your job type from the config panel (
-                  <CogIcon className="inline fill-current h-3 w-3 text-white" />
-                  ) in the top right.
-                </li>
-                <li className="py-1">
-                  Drag some tasks from the palette panel (
-                  <PlusIcon className="inline fill-current h-3 w-3 text-white" />
-                  ) and drop them onto the workspace area.
-                </li>
-                <li className="py-1">
-                  Connect the task nodes together and fill in any config fields.
-                </li>
-                <li className="py-1">
-                  Your generated job spec (in TOML format) will be ready for you
-                  in the code panel (
-                  <CodeBracketIcon className="inline fill-current h-3 w-3 text-white" />
-                  ). Any code highlighted in red here indicates missing or
-                  invalid configuration.
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      )} */}
-
       <main className="w-full h-full relative">
         <div className="w-full h-full fixed z-0">
           <Flow />
