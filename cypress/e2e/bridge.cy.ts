93aa8a110f2980c2bb4ac902c1afae2c14106115
Matt Durkin <m.durkin92@gmail.com>
2023-01-12 11:34:56 +0000

    Add test for bridge task which passes jobRun.meta var

--- cypress/e2e/bridge.cy.ts
+++ cypress/e2e/bridge.cy.ts
@@ -2,7 +2,8 @@ import { Test, generateTest } from "./task"
 import web3utils from "web3-utils"
 
 const tests: Array<Test> = [
-    { task: "bridge", name: "requestData (empty) + includeInputAtKey + meta", inputs: [{ value: "123.45", type: "decimal" }], options: {name: "bridge1", requestData: ``, includeInputAtKey: "input"}, want: { value: web3utils.utf8ToHex('{"input":"123.45"}'), type: "string" } },
+    { task: "bridge", name: "requestData (empty) + includeInputAtKey", inputs: [{ value: "123.45", type: "decimal" }], options: {name: "bridge1", requestData: ``, includeInputAtKey: "input"}, want: { value: web3utils.utf8ToHex('{"input":"123.45"}'), type: "string" } },
+    { task: "bridge", name: "requestData (empty) + includeInputAtKey + meta", inputs: [{ value: "123.45", type: "decimal" }], jobRun: { meta: { keep: { "theMeta": "yes" } }}, options: {name: "bridge1", requestData: ``, includeInputAtKey: "input"}, want: { value: web3utils.utf8ToHex('{"input":"123.45","meta":{"theMeta":"yes"}}'), type: "string" } },
 ]
 
 describe("bridge", () => {
--- cypress/e2e/task.ts
+++ cypress/e2e/task.ts
@@ -11,6 +11,8 @@ export type Test = {
     inputs: Array<Var>;
     options?: { [key: string]: any };
     vars?: { [key: string]: Var };
+    jobRun?: { [key: string]: Var };
+    jobSpec?: { [key: string]: Var };
     want?: Var;
     want64?: string;
     expectError?: boolean;
@@ -18,19 +20,21 @@ export type Test = {
 }
 
 export const generateTest = (test: Test, inputs64Override?: Array<string>) => {
-    return handleVarsConversion(test.vars, test.inputs)
+    return handleVarsConversion(test.vars, test.jobRun, test.jobSpec, test.inputs)
         .then((response) => {
             return performTask(test, response.body.vars64, inputs64Override || response.body.inputs64)
         })
 }
 
-const handleVarsConversion = (vars: Test["vars"], inputs: Test["inputs"]) => {
+const handleVarsConversion = (vars: Test["vars"], jobRun: Test["jobRun"], jobSpec: Test["jobSpec"], inputs: Test["inputs"]) => {
     return cy.request(
         "POST",
         "api/var-helper",
         {
-            ...vars && { vars: vars },
-            ...inputs && { inputs: inputs },
+            ...vars && { vars },
+            ...jobRun && { jobRun },
+            ...jobSpec && { jobSpec },
+            ...inputs && { inputs },
         },
     )
 }
