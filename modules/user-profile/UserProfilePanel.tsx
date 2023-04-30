b511c23ba6d7f241f7fce51203b2f745cddd54ea
Matt Durkin <m.durkin92@gmail.com>
2023-04-30 10:12:05 +0100

    Update UserProfilePanel.tsx

--- modules/user-profile/UserProfilePanel.tsx
+++ modules/user-profile/UserProfilePanel.tsx
@@ -1,11 +1,8 @@
-import { ExpanderPanel, Tooltip } from "../../components";
-import {
-    UserIcon
-} from "@heroicons/react/24/outline";
+import { Tooltip } from "../../components";
 import { GlobalStateContext } from "../../context/GlobalStateContext";
 import { useContext } from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useUser } from "@thirdweb-dev/react";
 
 export interface UserProfilePanelProps {
     className?: string;
@@ -16,20 +13,40 @@ export const UserProfilePanel = ({ className = "" }: UserProfilePanelProps) => {
 
     const queryClient = useQueryClient()
 
-    const { data, error, isLoading } = useQuery({
-        queryKey: ["getJobSpecs"], queryFn: () => fetch("/api/job-specs").then(res => {
+    const { isLoggedIn } = useUser()
+
+    const { data, error, isLoading, isFetching } = useQuery({
+        queryKey: ["getJobSpecs"], 
+        queryFn: () => fetch("/api/job-specs").then(res => {
             if (res.status !== 200) {
-                return new Error("An error occurred")
+                throw new Error("An error occurred")
             }
 
             return res.json()
-        })
+        }),
+        enabled: isLoggedIn
     })
 
     const handleLoadJobSpecVersion = (jsonContent: any) => {
         globalServices.workspaceService.send("RESTORE_STATE", {
             savedContext: jsonContent,
         })
+    }
+
+    const renderJobSpecs = ({ data, error, isLoading, isFetching }: { data: any, error: unknown, isLoading: boolean, isFetching: boolean }) => {
+        
+        if (!isFetching && !data && !error) return null
+
+        if (isLoading) return <span>Loading...</span>
+
+        if (error) return <span>An error occurred</span>
+
+        const hasNoJobSpecs = !isLoading && !error && !data
+        if (hasNoJobSpecs) return <span>No saved job specs... yet.</span>
+
+        return data.map((spec: any, index: number) => <ul key={index} onClick={() => handleLoadJobSpecVersion(spec.job_spec_versions[0].content)}>
+            {spec.job_spec_versions[0].name ?? `Unnamed ${index + 1}`}
+        </ul>)
     }
 
     return (
@@ -42,25 +59,25 @@ export const UserProfilePanel = ({ className = "" }: UserProfilePanelProps) => {
                     </p>
                 </Tooltip>
             </div>
-            <div className="flex-col items-center justify-end p-4 w-80">
-                <div className="py-8">
-                    <ConnectWallet
-                        btnTitle="Connect Wallet"
-                        theme="light"
-                        className="!bg-base-100 !text-white !rounded-full !p-4 !border-solid !border-2 !border-base-100 hover:!border-secondary focus:!ring-0"
-                        auth={{
-                            loginOptional: false,
-                            loginOptions: {
-                                domain: process.env.NEXT_PUBLIC_AUTH_DOMAIN
-                            }
-                        }}
-                    />
+            <div className="flex-col justify-end p-4 w-80 gap-8 flex">
+                <div className="flex justify-center">
+                    <div className="p-2 bg-white/10 rounded-lg">
+                        <ConnectWallet
+                            btnTitle="Connect Wallet"
+                            theme="light"
+                            className="!bg-base-100 !text-white !rounded-full !p-4 !border-solid !border-2 !border-base-100 hover:!border-secondary focus:!ring-0"
+                            auth={{
+                                loginOptional: false,
+                                loginOptions: {
+                                    domain: process.env.NEXT_PUBLIC_AUTH_DOMAIN
+                                }
+                            }}
+                        />
+                    </div>
                 </div>
-                <h4 className="font-bold">Your Job Specs</h4>
+                <h4 className={`${isLoggedIn ? "" : "text-gray-500"} font-bold`}>My Saved Job Specs</h4>
                 <ul>
-                    {isLoading ? "LOADING..." : error ? data.map((spec: any, index: number) => <ul key={index} onClick={() => handleLoadJobSpecVersion(spec.job_spec_versions[0].content)}>
-                        {spec.job_spec_versions[0].name ?? `Unnamed ${index + 1}`}
-                    </ul>) : <span>{error as string}</span>}
+                    {renderJobSpecs({ data, error, isLoading, isFetching })}
                 </ul>
             </div>
         </>
